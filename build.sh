bdeb8bb676fb7484c19ff0772316ace75f53dcbe
Dred <callmedredik@gmail.com>
2023-01-19 14:48:18 +0200

    added full configurate for docker and terraform

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker build -t base-jenkins ./base_image
+
+#docker build -t callmedredik/master-jenkins ./master_image 
+#docker run --name jenkins --rm -dp 8080:8080 --env JENKINS_ADMIN_ID=admin --env JENKINS_ADMIN_PASSWORD=password callmedredik/master-jenkins
